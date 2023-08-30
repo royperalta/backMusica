5b5d8d40aa536d01ab0b1d132309029fccacb450
royperalta <rrpb33@gmail.com>
2023-08-30 08:21:41 -0500

    creando el servidor

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("nuevo cambio")
+const express = require("express")
+
+const app = express()
+
+const PORT = 20000
+
+app.listen(PORT ,()=>{
+    console.log("El servidor esta corrriendo " + PORT)
+})
