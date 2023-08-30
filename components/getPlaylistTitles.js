9350a6c22263134aa765b32edf91cc5c0921ff64
royperalta <rrpb33@gmail.com>
2023-08-30 09:20:54 -0500

    cambios

--- components/getPlaylistTitles.js
+++ components/getPlaylistTitles.js
@@ -2,25 +2,45 @@ const { exec } = require('child_process');
 
 function listarTitulos() {
     // Reemplaza 'URL_DEL_PLAYLIST' con la URL real del playlist de YouTube que deseas analizar.
+    const { spawn } = require('child_process');
+
+    // Reemplaza 'URL_DEL_PLAYLIST' con la URL real del playlist de YouTube que deseas analizar.
     const playlistUrl = 'https://www.youtube.com/playlist?list=PL4fGSI1pDJn61j743B9r2LNeLCUUZsRMV';
 
-    // Ejecutar yt-dlp para obtener la información del playlist
-    const command = `yt-dlp --skip-download --get-title --flat-playlist ${playlistUrl}`;
+    // Ruta al archivo yt-dlp.exe
+    const ytDlpPath = './extensiones/yt-dlp.exe';
 
-    exec(command, (error, stdout, stderr) => {
-        if (error) {
-            console.error(`Error al obtener los títulos: ${error}`);
-            return;
-        }
+    // Argumentos para yt-dlp (obtener títulos de un playlist)
+    const args = [
+        '--skip-download',
+        '--get-title',
+        '--flat-playlist',
+        playlistUrl,
+    ];
 
-        // Los títulos se separan por líneas, así que los dividimos en un array
-        const titles = stdout.trim().split('\n');
+    // Ejecutar yt-dlp como un proceso hijo
+    const ytDlpProcess = spawn(ytDlpPath, args);
 
-        // Imprimir los títulos
+    // Capturar la salida estándar (stdout) del proceso
+    ytDlpProcess.stdout.on('data', (data) => {
+        const titles = data.toString().trim().split('\n');
+
         console.log('Títulos del playlist:');
         titles.forEach((title, index) => {
             console.log(`${index + 1}. ${title}`);
         });
+    });
+
+    // Capturar errores si los hay
+    ytDlpProcess.stderr.on('data', (data) => {
+        console.error(`Error al obtener los títulos: ${data}`);
+    });
+
+    // Manejar el cierre del proceso
+    ytDlpProcess.on('close', (code) => {
+        if (code !== 0) {
+            console.error(`Proceso de yt-dlp finalizado con código de salida ${code}`);
+        }
     });
 
 }
