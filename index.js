8179e910835806d259a4b5c828924d9a39d16d76
royperalta <rrpb33@gmail.com>
2023-11-11 02:04:06 -0500

    backend update

--- index.js
+++ index.js
@@ -1,21 +1,28 @@
-const dotenv = require('dotenv')
-dotenv.config()
-const express = require("express")
-const route = require("./routes.js")
-const cors = require("cors")
+const dotenv = require('dotenv');
+dotenv.config();
+const express = require('express');
+const https = require('https');
+const fs = require('fs');
+const route = require('./routes.js');
+const cors = require('cors');
 
-const app = express()
+const app = express();
 
-app.use(cors())
-app.use(express.json())
-app.use('/music', express.static('./downloads'))
-app.use("/api", route)
+app.use(cors());
+app.use(express.json());
+app.use('/music', express.static('./downloads'));
+app.use('/api', route);
 
+// Configuración para el servidor HTTPS
+const httpsOptions = {
+  key: fs.readFileSync('/etc/ssl/virtualmin/1692085281313966/ssl.key'), // Reemplaza con la ruta a tu clave privada
+  cert: fs.readFileSync('/etc/ssl/virtualmin/1692085281313966/ssl.cert'), // Reemplaza con la ruta a tu certificado
+};
 
+const httpsServer = https.createServer(httpsOptions, app);
 
-const PORT = 20000
+const PORT = 9100; // Cambié el puerto a 9100
 
-app.listen(PORT, () => {
-    console.log("El servidor esta corrriendo " + PORT)
-})
-
+httpsServer.listen(PORT, () => {
+  console.log('El servidor está corriendo en el puerto ' + PORT);
+});
